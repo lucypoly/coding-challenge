9e653fecf6f0b2785133e417e202b6d7fdceca41
liudmyla.polianychko <liudmyla.polianychko@leaseplan.com>
2019-10-30 12:25:38 +0200

    Updates

---
+++ precache-manifest.994dd7d676aaf92a4bae2c399cd3998e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0d051ac1049db53c74732169703b6f",
+    "url": "/coding-challenge/index.html"
+  },
+  {
+    "revision": "c8bd4da00f5ccfe32b8d",
+    "url": "/coding-challenge/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "c8bd4da00f5ccfe32b8d",
+    "url": "/coding-challenge/static/js/2.97991123.chunk.js"
+  },
+  {
+    "revision": "e55cb260a77f859b00b5",
+    "url": "/coding-challenge/static/js/main.ebe45e4f.chunk.js"
+  },
+  {
+    "revision": "5a4ddacdf5155d024020",
+    "url": "/coding-challenge/static/js/runtime-main.9ffb5cae.js"
+  }
+]);
